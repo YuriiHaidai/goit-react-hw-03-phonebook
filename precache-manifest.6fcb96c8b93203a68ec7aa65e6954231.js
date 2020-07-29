d6e07588e75bb70c50a2979f13895d63111b5133
Gaiday_u <ura.kiev@gmail.com>
2020-07-29 10:39:39 +0300

    Updates

---
+++ precache-manifest.6fcb96c8b93203a68ec7aa65e6954231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9c3b5a1c05de5d63bebe47b60e1964",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "5592d4453b3539df5051",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.f9c00974.chunk.css"
+  },
+  {
+    "revision": "06738f72776f48ca4373",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.aca390e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.aca390e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5592d4453b3539df5051",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.3f534b62.chunk.js"
+  },
+  {
+    "revision": "412989084c224794c988",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.77bb1b5a.js"
+  }
+]);
